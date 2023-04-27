d8912992212dd87ec3ecf11ef8199cfaab7d988e
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-04-27 21:19:48 +0530

    Add project files.

---
+++ BankingProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankingProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankingProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6ac366c-0e79-4ec2-970d-ac5dbc85ad06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BankingProject/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Reflection;
+
+namespace BankingProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Launching browser")]
+        public void TestMethod1()
+        {
+            //launching the browser
+            IWebDriver driver = new ChromeDriver();
+            //opening the URL
+            driver.Url = "https://www.google.com/";
+            //maximize the window
+            driver.Manage().Window.Maximize();
+            //getting pagesource
+            var data=driver.PageSource;
+            Console.WriteLine(data);
+            Console.WriteLine("----------------------------------------");
+            //getting the title of the web page
+            var title= driver.Title;
+            Console.WriteLine("Title of the page is:  "+title);
+            Console.WriteLine("----------------------------------------");
+
+            //address of the wiindow
+            var windowID = driver.CurrentWindowHandle;
+            Console.WriteLine("Window Id is :"+windowID);
+            Console.WriteLine("----------------------------------------");
+
+            //var element = driver.FindElement(By.Name(""));
+        }
+
+    }
+}

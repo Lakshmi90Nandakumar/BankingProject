6de7401ebaf78ffc6c370aa906c1949e81280336
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-04-28 12:27:19 +0530

    authendication pop up class added

---
+++ BankingProject/Practise/AuthendicationPrac.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace BankingProject.Practise
+{
+    [TestClass]
+    public class AuthendicationPrac
+    {
+        [TestMethod]
+        [TestCategory("to handle authendication pop up")]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://admin:admin@the-internet.herokuapp.com/basic_auth";
+            String text=driver.FindElement(By.XPath("//a[contains(.,'Elemental Selenium')]")).Text;
+            if(text.Contains("Selenium"))
+            {
+              Console.WriteLine("Successfully logggedin");
+            
+            }
+            else
+            {
+                Console.WriteLine("Not Successful");
+            }
+        }
+    }
+}
--- BankingProject/Practise/FaceBookPrac.cs
+++ BankingProject/Practise/FaceBookPrac.cs
@@ -18,27 +18,34 @@ namespace BankingProject.Practise
         [TestCategory("facebookAutomation")]
         public void testMethod()
         {
+            //lauching the browser
             IWebDriver driver=new ChromeDriver();
+            //enter the url
             driver.Url = "https://www.facebook.com/";
+            //wait statement
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //inspecting elements and handling
             driver.FindElement(By.XPath("//a[contains(.,'Create new account')]")).Click();
             driver.FindElement(By.Name("firstname")).SendKeys("lakshmi");
             driver.FindElement(By.Name("lastname")).SendKeys("nanda");
             driver.FindElement(By.Name("reg_email__")).SendKeys("lakshminanda@gmail.com");
             driver.FindElement(By.Name("reg_email_confirmation__")).SendKeys("lakshminanda@gmail.com");
             driver.FindElement(By.Id("password_step_input")).SendKeys("Nanda@123");
+           
             IWebElement day=driver.FindElement(By.Name("birthday_day"));
-            SelectElement s=new SelectElement(day);
+            SelectElement s=new SelectElement(day);//using selectElement class to click day list box
             s.SelectByText("10");
-            IWebElement month = driver.FindElement(By.Name("birthday_month"));
+           
+            IWebElement month = driver.FindElement(By.Name("birthday_month"));//using selectElement class to click month list box
             SelectElement s1 = new SelectElement(month);
             s1.SelectByText("May");
-            IWebElement year = driver.FindElement(By.Name("birthday_year"));
+           
+            IWebElement year = driver.FindElement(By.Name("birthday_year"));//using selectElement class to click year list box
             SelectElement s2 = new SelectElement(year);
             s2.SelectByText("1990");
-            driver.FindElement(By.XPath("//label[text()='Female']")).Click();
-            driver.FindElement(By.Name("websubmit")).Click();
-            driver.FindElement(By.XPath("//span[contains(.,'Continue')]")).Click();
+            driver.FindElement(By.XPath("//label[text()='Female']")).Click();//click on gender radio button
+            driver.FindElement(By.Name("websubmit")).Click();//click on  submit
+            //driver.FindElement(By.XPath("//span[contains(.,'Continue')]")).Click();
 
 
         }
